38220caaee9fd0baa35810dfd191bfd7b6d1bd2f
joelruivocad <joel.ruivo@cadastra.com>
2026-04-01 11:59:57 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,3 @@
-// ═══════════════════════════════════════════
-// CNAcademy — Firebase Config
-// ═══════════════════════════════════════════
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
